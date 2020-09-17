c51e6203b5ae7a4531fc30d6d88ee97fd1dca4b7
Nils Edvardsson <Nils.Edvardsson@hiq.se>
2020-09-17 12:25:22 +0200

    first commit

---
+++ DockerWebApi/Controllers/NamesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DockerWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NamesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<NamesController> _logger;
+
+        public NamesController(ILogger<NamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            Random random = new Random();
+            var randomNumber = random.Next(1, 6);
+            return new[]
+            {
+                Environment.MachineName,
+                randomNumber.ToString()
+            };
+        }
+    }
+}
